acfa58e0f0c7711cfc0c6d151c14560b54692105
theQuantumMechanic <albertferguson118@gmail.com>
2022-02-18 13:25:32 +1100

    added Database singleton, infra layer, models and BaseModel. Mock exec's repo added too

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+"""
+.. module:: Models
+    :platform: Unix, Windows
+    :synopsis: Crafty Controller Data Models (see CLEAN and Domain Driven Design)
+.. moduleauthor:: Albert Ferguson <albertferguson118@gmail.com>
+
+All codebase infrastructure resides here. Nothing presentation layer!
+"""
+
+__author__ = "Albert Ferguson"
+
+all = []
